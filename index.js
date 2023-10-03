4d14d4941ef3d8d023203da0465ba265effb3d0a
karl <karl.dyyna@gmail.com>
2023-10-03 11:46:47 +0300

    v2.0.2 - change typescript target

--- index.js
+++ index.js
@@ -2,28 +2,34 @@
  * Created by karl on 14/07/15.
  */
 "use strict";
-export function init(express) {
-    function success(data = "Done", status = 200) {
-        const message = {
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.init = void 0;
+function init(express) {
+    function success(data, status) {
+        if (data === void 0) { data = "Done"; }
+        if (status === void 0) { status = 200; }
+        var message = {
             status: "success",
-            data,
+            data: data,
         };
         if (this.req.log) {
-            this.req.log.trace({ message }, "Sending success response");
+            this.req.log.trace({ message: message }, "Sending success response");
         }
         this.status(status).json(message);
     }
-    function fail(data, status = 400) {
-        const message = {
+    function fail(data, status) {
+        if (status === void 0) { status = 400; }
+        var message = {
             status: "fail",
-            data,
+            data: data,
         };
         if (this.req.log) {
-            this.req.log.trace({ message }, "Sending fail response");
+            this.req.log.trace({ message: message }, "Sending fail response");
         }
         this.status(status).json(message);
     }
-    function error(err, status = 500) {
+    function error(err, status) {
+        if (status === void 0) { status = 500; }
         if (typeof err !== "object") {
             err = {
                 message: err,
@@ -35,11 +41,12 @@ export function init(express) {
         }
         this.status(err.code || status).json(err);
     }
-    function partial(data, status = 206) {
+    function partial(data, status) {
+        if (status === void 0) { status = 206; }
         if (!data.limit && data.data && data.data.length) {
             data.limit = data.offset + (data.data.length - 1);
         }
-        const header = data.offset + "-" + data.limit + "/" + data.count;
+        var header = data.offset + "-" + data.limit + "/" + data.count;
         this.header("Content-Range", header);
         if (this.req.log) {
             this.req.log.trace({ header: header }, "Sending partial response");
@@ -47,9 +54,10 @@ export function init(express) {
         this.success(data.data, status);
     }
     Object.assign(express.response, {
-        success,
-        fail,
-        error,
-        partial,
+        success: success,
+        fail: fail,
+        error: error,
+        partial: partial,
     });
 }
+exports.init = init;
